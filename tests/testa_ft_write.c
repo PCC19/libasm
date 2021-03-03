bdca4341df0edbacf465abba0091022fa3ce0a7a
Paulo Cunha <pcunha00@hotmail.com>
2021-03-04 00:34:53 +0100

    testa_ft_write

--- tests/testa_ft_write.c
+++ tests/testa_ft_write.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 22:40:01 by user42            #+#    #+#             */
-/*   Updated: 2021/03/03 22:51:06 by user42           ###   ########.fr       */
+/*   Updated: 2021/03/04 00:33:48 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ static void	print_o(size_t count, int fd, char *s1)
 {
 	size_t	n1;
 
+	errno = 0;
 	if (fd > 1)
 		fd = open("aw", O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	n1 = write(fd, s1, count);
 	printf("======= ft_write  =======\n");
 	printf("   write s1: |%s|\t\t n1: %zu\t\t errno: %d\n", s1, n1, errno);
-	if (fd != 0)
+	if (fd != 1)
 		close(fd);
 }
 
@@ -30,24 +31,24 @@ static void	print(size_t count, int fd, char *s1)
 {
 	size_t	n1;
 
+	errno = 0;
 	if (fd > 1)
 		fd = open("aw", O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	n1 = write(fd, s1, count);
-	printf("ft_read s1: |%s|\t\t n1: %zu\t\t errno: %d\n", s1, n1, errno);
-	if (fd != 0)
+	printf("ft_write s1: |%s|\t\t n1: %zu\t\t errno: %d\n", s1, n1, errno);
+	if (fd != 1)
 		close(fd);
 }
 
 void		testa_ft_write(void)
 {
 	printf("STDOUT (1)\n");
-	print_o(0, 1, "Teste!");
-	print(0, 1, "Teste!");
+	print_o(3, 1, "Teste!");
+	print(3, 1, "Teste!");
 	printf("OPEN FILE\n");
-	print_o(1, 1, "Teste!");
-	print(1, 1, "Teste!");
+	print_o(3, 2, "Teste!");
+	print(3, 2, "Teste!");
 	printf("Wrong fd (-1)\n");
-	print_o(10, -5, "Teste!");
-	print(10, -5, "Teste!");
+	print_o(5, -5, "Teste!");
+	print(5, -5, "Teste!");
 }
-
